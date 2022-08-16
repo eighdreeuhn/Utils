1c77524f68716c4ff5988302d90cbe8596d10e39
Adrian Gioia <afgioia@gmail.com>
2022-08-15 22:12:13 -0700

    minor changes

--- frequency_reduce.py
+++ frequency_reduce.py
@@ -2,4 +2,4 @@ from functools import reduce
 
 
 def frequency(word):
-    return reduce(lambda freq, char: {**freq, char: freq[char]+1} if char in freq else {**freq, char: 1} , [*word], {})
+    return reduce(lambda freq, char: {**freq, char: freq[char]+1} if char in freq else {**freq, char: 1}, [*word], {})
