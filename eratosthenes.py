66bbed7cef9084b7c50cfb58b184c8568fc41391
Adrian Gioia <afgioia@gmail.com>
2022-08-21 12:54:26 -0700

    Wrote out the Sieve of Eratosthenes for generating primes up to N

---
+++ eratosthenes.py
@@ -0,0 +1,16 @@
+
+#   Sieve of Eratosthenes method for generating primes up to n     #
+
+
+def eratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while p**2 <= n:
+        if prime[p]:
+            for i in range(p**2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime = [n for n in range(1, n) if prime[n]]
+    return prime
+
+print(eratosthenes(1000))
