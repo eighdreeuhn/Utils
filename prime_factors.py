bcb656e45c89691c5498119c0483fada48329167
Adrian Gioia <afgioia@gmail.com>
2022-11-06 20:04:09 -0800

    Updated algorithm

--- prime_factors.py
+++ prime_factors.py
@@ -1,16 +1,15 @@
-from is_prime import is_prime
-
-
 def prime_factors(n):
-    factors = {}
-    while n != 1:
-        for i in range(2, n + 1):
-            if n % i == 0:
-                factors[i] = 1 if not i in factors else factors[i] + 1
-                n = n // i
-                break
+    factors, f = {}, 2
+    while f <= n:
+        while n%f == 0:
+            if f in factors:
+                factors[f] += 1
+                n /= f
+            else:
+                factors[f] = 1
+                n /= f
+        f += 1
     return factors
 
 
-print(is_prime(8276447))
-print(prime_factors(9198323463))
+print(prime_factors(9192777468))
