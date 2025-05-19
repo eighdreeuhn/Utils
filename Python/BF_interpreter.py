c3b1c0a583026f92689b990b2a42b9dd9e2ddaeb
Adrian Gioia <afgioia@gmail.com>
2025-05-19 08:16:17 -0500

    Working Version-1.

--- Python/BF_interpreter.py
+++ Python/BF_interpreter.py
@@ -10,19 +10,26 @@ def brainfuck_programs(length=9):
     chars = "+-<>[]."
     for program in product(chars, repeat=length):
         p = ''.join(program)
-        if p[0] in "[]." or p.count('[') != p.count(']'):
+        if p[0] in "[]." or "<>" in p or "><" in p or "+-" in p or "-+" in p or p.count('[') != p.count(']'):
             continue
         yield p
 
 
 def beaver_hunt():
+    global BEST, WINNER
     gen = brainfuck_programs()
-    for program in gen:
-        run_bf(program)
+    for i, program in enumerate(gen):
+        print(program)
+        result = run_bf(program)
+        print(result)
+        if result > BEST:
+            BEST = result
+            WINNER = program
+            print(f"NEW LEADER: {program} CLOCKED IN AT {result} STEPS!")
+    print(WINNER, BEST)
 
 
 def run_bf(code):
-    global BEST, WINNER
     jump_table = {}
     stack = []
     for i, t in enumerate(code):
@@ -40,16 +47,31 @@ def run_bf(code):
     idx   = 0
     steps = 0
     while True:
-        if ptr >= 4000 or ptr < 0:
+        if ptr >= 4_000 or ptr < 0 or steps > 10_000:
             return 0
         if idx == len(code):
             return steps
+
         match code[idx]:
             case '+':
                 tape[ptr] = (tape[ptr] + 1) % 256
             case '-':
                 tape[ptr] = (tape[ptr] - 1) % 256
-        continue
+            case '>':
+                ptr += 1
+            case '<':
+                ptr -= 1
+            case '[':
+                if not tape[ptr]:
+                    idx = jump_table[idx]
+            case ']':
+                if tape[ptr]:
+                    idx = jump_table[idx]
+            case '.':
+                pass
+
+        idx += 1
+        steps += 1
 
 
 beaver_hunt()
